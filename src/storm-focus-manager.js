49c514b24fe6427da1c73defe640d445ba17008e
Mick Perkins <mickperkins@gmail.com>
2016-02-20 20:28:34 +0000

    Refactoring

--- src/storm-focus-manager.js
+++ src/storm-focus-manager.js
@@ -72,18 +72,36 @@
 		getFocusableChildren: function() {
 			var focusableElements = ['a[href]', 'area[href]', 'input:not([disabled])', 'select:not([disabled])', 'textarea:not([disabled])', 'button:not([disabled])', 'iframe', 'object', 'embed', '[contenteditable]', '[tabindex]:not([tabindex="-1"])'];
 
-			return [].slice.call(this.node.querySelectorAll(focusableElements.join(','))).filter(function (child) {
+			this.focusableChildren = [].slice.call(this.node.querySelectorAll(focusableElements.join(','))).filter(function (child) {
 			  return !!(child.offsetWidth || child.offsetHeight || child.getClientRects().length);
 			});
 		},
 		addListeners: function() {
+			//trigger this once the modal/toggler is open
 			document.addEventListener('keydown', this.keyListener.bind(this), true);
 			//document.body.addEventListener('focus', this.focusListener.bind(this), true);
 		},
-		removeListeners = function() {
+		removeListeners: function() {
+			//trigger this once the modal/toggler is closed
 			document.removeEventListener('keydown', this.keyListener);
 			//document.removeEventListener('focus', this.focusListener);
 		},
+		keyListener: function(e) {
+			if(e.keyCode === 9) {
+				this.catchTab(e);
+			}
+		},
+		catchTab: function(e) {
+			var focusedItemIndex = this.focusableChildren.indexOf(document.activeElement);
+		
+			if (e.shiftKey && focusedItemIndex === 0) {
+			  this.focusableChildren[this.focusableChildren.length - 1].focus();
+			  e.preventDefault();
+			} else if (!event.shiftKey && focusedItemIndex === this.focusableChildren.length - 1) {
+			  this.focusableChildren[0].focus();
+			  e.preventDefault();
+			}
+		}
 	};
 	
  }));
